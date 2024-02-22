f2f8206347514a1cc251daf7fa72fd447d7301a1
vijay <vijay.mangapoti@gmail.com>
2024-02-22 23:38:59 +0530

    Updated key maps description

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,8 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jk", "<Esc>", { desc = 'Press "jk" to enter normal mode' })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down with cursor at center" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up with cursor at center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move the cursor down while keeping it centered." })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move the cursor up while keeping it centered." })
 vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
 vim.keymap.set("n", "<C-N>", "<cmd>NvimTreeFocus<cr>", { desc = "Focus NvimTree" })
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
