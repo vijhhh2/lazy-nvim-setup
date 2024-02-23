b5c662f05edb16d17b5f18a3078861f04dad53fb
vijay <vijay.mangapoti@gmail.com>
2024-02-23 18:55:36 +0530

    Changed from nvim tree to neo tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
 vim.keymap.set("i", "jk", "<Esc>", { desc = 'Press "jk" to enter normal mode' })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move the cursor down while keeping it centered." })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move the cursor up while keeping it centered." })
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
